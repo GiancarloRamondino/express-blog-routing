7c2675eb030c653369bcaa4534928d7a719ad2ac
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-04-17 15:55:39 +0200

    [UPD] router

--- app.js
+++ app.js
@@ -6,60 +6,17 @@ const app = express();
 const port = 3000;
 
 //imposto asset statici
-app.use(express.static('public'));
+app.use(express.static('post'));
+
+//importo il router
+const postsRouter = require('./router.js'); 
+
+// uso post router per creare le rotte
+app.use('/router', postsRouter);
 
 //definisco la rotta principale
 app.get('/', (req, res) => {
   res.send('Server del mio blog!');
-});
-
-//definisco la rotta per il blog
-app.get('/bacheca', (req, res) => {
-    const menu = [{
-        title: "ciambellone",
-        contenuto: "Ecco la ricetta del ciambellone",
-        images: 'ciambellone.jpeg',
-        tag: ['home', 'blog', 'contatti']
-    },
-    {
-        title: "cracker",
-        contenuto: "Ecco la ricetta dei cracker",
-        images: 'cracker_barbabietola.jpeg',
-        tag: ['home', 'blog', 'contatti']
-    },
-    {
-        title: "pane fritto",
-        contenuto: "Ecco la ricetta del pane fritto",
-        images: 'pane_fritto_dolce.jpeg',
-        tag: ['home', 'blog', 'contatti']
-    },
-    {
-        title: "pasta  alla barbabietola",
-        contenuto: "Ecco la ricetta della pasta alla barbabietola",
-        images: 'pasta_barbabietola.jpeg',
-        tag: ['home', 'blog', 'contatti']
-    }, 
-    {
-        title: "torta paesana",
-        contenuto: "Ecco la ricetta della torta paesana",
-        images: 'torta_paesana.jpeg',
-        tag: ['blog', 'contatti']
-    }];
-
-    // Applico un filtro per il titolo
-    const title = req.query.title;
-    // Dichiarazione della variabile per il menu filtrato
-    let filteredMenu;
-    // Verifico se la variabile title contiene un valore e non è indefinita
-    if (title) {
-        filteredMenu = menu.filter((item) => {
-            return item.title.includes(title);
-        });
-    } else {
-        filteredMenu = menu;
-    }
-    
-    res.json(filteredMenu);
 });
 
 //server che ascolta sulla porta 3000
--- router.js
+++ router.js
@@ -8,12 +8,27 @@ router.get('/', function(req, res) {
 
 //show
 router.get('/:id', function(req, res) {
-    res.send('dettagli del menu!');
+    res.send('dettagli del menu!'+req.params.id);
 });
 
 //store
 router.post('/', function(req, res) {
     res.send('creazione del menu!');
+});
+
+//update
+router.put('/', function(req, res) {
+    res.send('modifica del menu!');
+});
+
+//modifica
+router.patch('/', function(req, res) {
+    res.send('modifica parziale del menu!');
+});
+
+//delete
+router.delete('/', function(req, res) {
+    res.send('cancellazione del menu!');
 });
 
 module.exports = router;
